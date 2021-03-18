e3bd13e77bc20147b304c3d1857eb832ab8f1fee
Andres Suarez <andres27059934@gmail.com>
2021-03-17 22:36:02 -0300

    Hotfix 1.3.1

--- src/services/graphql/resolvers/users/userMutationResolver.ts
+++ src/services/graphql/resolvers/users/userMutationResolver.ts
@@ -6,11 +6,5 @@ import db from '../../../firebase/firebaseDb'
 // TODO: Define the UserInput type
 export async function createUser(userInput: UserInput): Promise<User> {
     User.validateUserInput(userInput)
-
-    // TODO: Wrap this to all the project using generics
-    const dbResponse = db.ref().child('users').push({ ...userInput })
-    const newUserEntry = (await (await dbResponse).get()).val()
-    const newUserId = (await dbResponse).key
-
-    return createOnFirebaseDB<User>('users', {id: newUserId, ...newUserEntry}, User)
+    return createOnFirebaseDB<User>('users', userInput, User)
 }
