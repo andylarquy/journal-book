d4d618df4cec664a3ce6dd0b5f3aa7955c29a5a5
andylarquy <andres27059934@gmail.com>
2021-03-17 21:39:32 -0300

    Revert "JB-56500 First approach to reuse firbase create calls"

--- src/services/graphql/resolvers/users/userMutationResolver.ts
+++ src/services/graphql/resolvers/users/userMutationResolver.ts
@@ -1,8 +1,14 @@
-import { createOnFirebaseDB } from '../../../firebase/firebaseDbUtils'
 import { User } from '../../../../domain/User'
+import db from '../../../firebase/firebaseDb'
 
 // TODO: Define the UserInput type
 export async function createUser(userInput: any): Promise<User> {
     User.validateUserInput(userInput)
-    return createOnFirebaseDB<User>('users', { ...userInput }, User)
+
+    // TODO: Wrap this to all the project using generics
+    const dbResponse = db.ref().child('users').push({ ...userInput })
+    const newUserEntry = (await (await dbResponse).get()).val()
+    const newUserId = (await dbResponse).key
+
+    return User.createFrom({id: newUserId, ...newUserEntry})
 }
